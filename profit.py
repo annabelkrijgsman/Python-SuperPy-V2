6832f50bf98b4443c7ed78da8f6a82782f1d2286
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2022-03-03 01:56:10 +0100

    Create profit.py

---
+++ profit.py
@@ -0,0 +1,9 @@
+from revenue import get_total_revenue
+from rich import print
+
+# Write functions like down below for calculating profit
+# def get_total_purchase():
+#     return 10
+
+# def get_total_profit():
+#     return get_total_revenue - get_total_purchase
